dc3d7a4ee4c0bd62d183627a42b8d402624c65e0
Rakeshg508 <rakesh_gupta508@yahoo.com>
2023-06-28 13:18:59 +0530

    Update maven-install.ps1

--- maven-install.ps1
+++ maven-install.ps1
@@ -1,6 +1,7 @@
 $mavenDownloadUrl = "https://archive.apache.org/dist/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.zip"
 $mavenArchive = "apache-maven.zip"
 $mavenHome = "$env:ProgramFiles\Apache\maven-3.8.4"
+$envvar = $mavenHome +"\apache-maven-3.8.4"
 
 Write-Host "Downloading Apache Maven..."
 Invoke-WebRequest -Uri $mavenDownloadUrl -OutFile $mavenArchive
@@ -8,8 +9,8 @@ Invoke-WebRequest -Uri $mavenDownloadUrl -OutFile $mavenArchive
 Write-Host "Extracting Apache Maven..."
 Expand-Archive -Path $mavenArchive -DestinationPath $mavenHome -Force
 Write-Host "Setting MAVEN_HOME and adding Maven to PATH..."
-[Environment]::SetEnvironmentVariable("MAVEN_HOME", $mavenHome, "Machine")
-[Environment]::SetEnvironmentVariable("Path", "$($env:Path);$mavenHome\bin", "Machine")
+[Environment]::SetEnvironmentVariable("MAVEN_HOME", $envvar, "Machine")
+[Environment]::SetEnvironmentVariable("Path", "$($env:Path);$envvar\bin", "Machine")
 
 Write-Host "Cleaning up Maven archive..."
 Remove-Item $mavenArchive
