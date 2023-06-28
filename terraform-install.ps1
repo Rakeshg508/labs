ac042199352434cc77fc1f34791ceb1f0e4e107f
Rakeshg508 <rakesh_gupta508@yahoo.com>
2023-06-28 22:13:44 +0530

    Update terraform-install.ps1

--- terraform-install.ps1
+++ terraform-install.ps1
@@ -1,7 +1,8 @@
 $TerraformVersion = "1.5.1"
-$TerraformZipUrl = "https://releases.hashicorp.com/terraform/$TerraformVersion/terraform_$TerraformVersion\_windows_amd64.zip"
+$TerraformZipUrl = "https://releases.hashicorp.com/terraform/$TerraformVersion/terraform_$TerraformVersion"+"_windows_amd64.zip"
 $TerraformZipFile = "terraform.zip"
-$TerraformExtractPath = "."
+$TerraformExtractPath = "terraform-path"
+
 
 # Create Terraform directory
 Write-Host "Create Terraform directory"
@@ -15,12 +16,19 @@ Invoke-WebRequest -Uri $TerraformZipUrl -OutFile $TerraformZipFile
 Write-Host "Extract Terraform zip"
 Expand-Archive -Path $TerraformZipFile -DestinationPath $TerraformExtractPath
 
+$currentDirectory = (Get-Location).Path
+Write-Host "Current Directory: $currentDirectory"
+
+$TerraformPath = $currentDirectory +"\" + $TerraformExtractPath 
+
+Write-Host "Terraform path Directory: $terraformPath" 
+
 # Add Terraform to system PATH
-$env:Path += ";$TerraformExtractPath"
+#$env:Path += ";$TerraformExtractPath"
+[Environment]::SetEnvironmentVariable("Path", "$($env:Path);$TerraformPath", "Machine")
 
 # Clean up Terraform zip file
 Write-Host "Clean up Terraform zip file"
 Remove-Item -Path $TerraformZipFile
 
 Write-Host "Terraform installation completed. Please make sure to reopen your PowerShell session for the changes to take effect."
-
