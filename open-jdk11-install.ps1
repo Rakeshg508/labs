34b1a071ebdb6a30586badb44d55bda3b36551bb
Rakeshg508 <rakesh_gupta508@yahoo.com>
2023-06-28 13:00:37 +0530

    Update open-jdk11-install.ps1

--- open-jdk11-install.ps1
+++ open-jdk11-install.ps1
@@ -1,6 +1,7 @@
 $openjdkDownloadUrl = "https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip"
 $openjdkArchive = "openjdk-11.0.2_windows-x64_bin.zip"
-$javaHome = "$env:ProgramFiles\Java\jdk-11\jdk-11.0.2"
+$javaHome = "$env:ProgramFiles\Java\jdk-11"
+$envvar = "$javaHome:jdk-11.0.2"
 
 Write-Host "Downloading OpenJDK 11..."
 Invoke-WebRequest -Uri $openjdkDownloadUrl -OutFile $openjdkArchive
@@ -8,8 +9,8 @@ Invoke-WebRequest -Uri $openjdkDownloadUrl -OutFile $openjdkArchive
 Write-Host "Extracting OpenJDK 11..."
 Expand-Archive -Path $openjdkArchive -DestinationPath $javaHome -Force
 Write-Host "Setting JAVA_HOME environment variable..."
-[Environment]::SetEnvironmentVariable("JAVA_HOME", $javaHome, "Machine")
-[Environment]::SetEnvironmentVariable("Path", "$($env:Path);$javaHome\bin", "Machine")
+[Environment]::SetEnvironmentVariable("JAVA_HOME",$envvar, "Machine")
+[Environment]::SetEnvironmentVariable("Path", "$($env:Path);$envvar\bin", "Machine")
 
 Write-Host "Cleaning up..."
 Remove-Item $openjdkArchive
